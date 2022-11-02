90ba8c71f01ac58beb1b3b6d304b81c2574eb7e8
Mauricio da Motta Braga <professormauriciodamotta@gmail.com>
2022-11-02 17:44:48 -0300

    Primeiras 5 frases adicionadas.

---
+++ HelloWorldGit/src/FrasesMotivacionais.java
@@ -0,0 +1,17 @@
+
+public class FrasesMotivacionais {
+		public static void main(String[] args) {
+
+			System.out.println("Exemplo Git frases motivacionais\n");
+			System.out.println("1. O medo de cair não pode ser maior que a paixão de voar.");
+			System.out.println("2. Se ainda venta não adianta varrer. Espere o momento certo de agir.");
+			System.out.println("3. O fim de um ciclo sempre traz o início de um novo caminho.");
+			System.out.println("4. Não importa o quão lento você vá desde que você não pare.");
+			System.out.println("5. Comece onde você está. Use o que você tem. Faça o que você pode.");
+			System.out.println("\nFim das frases motivacionais.");
+
+		}
+
+	}
+
+
